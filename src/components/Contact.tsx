da42bea6b76ef8577c083a2ae211f2367d174e27
Normi333 <shisirthapa146@gmail.com>
2025-10-29 23:08:00 +0545

    fix:emailjs fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -29,8 +29,7 @@ const Contact = () => {
             await emailjs.send(
                 import.meta.env.VITE_EMAILJS_SERVICE_ID,
                 import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-                templateParams,
-                import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+                templateParams
             )
 
             setSubmitStatus('success')
